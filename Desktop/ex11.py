b89578cc3497ad778532e667aa710be028789f72
Lena <lena@Lena>
2013-05-02 00:12:48 -0700

    adding ex 11 after looking for extra stuff online

---
+++ Desktop/ex11.py
@@ -0,0 +1,20 @@
+print "How old are you?",
+age  = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "how much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+   age, height, weight)
+
+name = raw_input('Enter your name : ')
+print ("Hi %s, Let us be friends!" % name)
+
+number = raw_input("What's your number?")
+print ("hey you're %s!" % number)
+
+x = int(raw_input("what's your favorite number?"))
+print ("hey %r is my favorite too!!!" % x)
+
+
